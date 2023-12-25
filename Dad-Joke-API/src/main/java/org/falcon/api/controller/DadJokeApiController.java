b5de54bee2eb886b97cd6483caa38c7a4022931a
ludwig <sigwartholiver2@gmail.com>
2023-12-25 10:34:41 -0600

    Refactor to use a service
    Took 18 seconds

--- Dad-Joke-API/src/main/java/org/falcon/api/controller/DadJokeApiController.java
+++ Dad-Joke-API/src/main/java/org/falcon/api/controller/DadJokeApiController.java
@@ -1,6 +1,8 @@
 package org.falcon.api.controller;
 
+import org.falcon.api.service.DadJokeService;
 import org.falcon.jokegenerator.DadJokeGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class DadJokeApiController {
-    DadJokeGenerator dadJokeGenerator = new DadJokeGenerator();
+    @Autowired
+    DadJokeService dadJokeGenerator;
     @GetMapping("/joke")
     public String getJoke() {
-        this.dadJokeGenerator.fetchJoke();
-        return this.dadJokeGenerator.getJoke().toString();
+        this.dadJokeGenerator.getDadJokeGenerator().fetchJoke();
+        return this.dadJokeGenerator.getJoke();
     }
 }
