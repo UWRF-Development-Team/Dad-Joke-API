2f43bcb4b25150699e4d834f48583f623a87ff49
ludwig <sigwartholiver2@gmail.com>
2023-12-25 10:33:34 -0600

    Implement partial functionality
    Took 45 seconds

--- Dad-Joke-API/src/main/java/org/falcon/jokegenerator/AiApiCall.java
+++ Dad-Joke-API/src/main/java/org/falcon/jokegenerator/AiApiCall.java
@@ -1,4 +1,94 @@
 package org.falcon.jokegenerator;
 
+import org.falcon.ApiModel;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class AiApiCall {
+    private String apiKey;
+    private String url;
+    private AiApiModel aiApiModel;
+    private String response;
+    private String requestBody;
+    //----------------------------Constructors--------------------------------
+    public AiApiCall(String apiKey, String url, AiApiModel aiApiModel) {
+        this.apiKey = apiKey;
+        this.url = url;
+        this.aiApiModel = aiApiModel;
+        this.buildRequestBody();
+        this.fetchResponse();
+    }
+    //-------------------------Build-Request-Body-----------------------------
+    public void buildRequestBody() {
+        this.requestBody = String.format("""
+                {
+                    "model": "%s",
+                    "prompt": "%s",
+                    "max_tokens": %d,
+                    "temperature": %1.1f
+                }""", this.aiApiModel.getModel(),
+                this.aiApiModel.getPrompt(),
+                this.aiApiModel.getMaxTokens(),
+                this.aiApiModel.getTemperature());
+    }
+    //---------------------------Fetch-Response-------------------------------
+    public void fetchResponse() {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        //------------------------Make-Request--------------------------------
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(this.url))
+                .setHeader("Authorization", "Bearer " + this.apiKey)
+                .setHeader("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(this.requestBody))
+                .build();
+        //-------------------------Get-Response-------------------------------
+        HttpResponse<String> responseHttp = null;
+        try {
+            responseHttp = httpClient.send(httpRequest,
+                    HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        if (responseHttp != null) {
+            this.response = responseHttp.body();
+        } else {
+            this.response = "Failed to execute request.";
+        }
+    }
+    //------------------------------Getters-----------------------------------
+    public String getApiKey() {
+        return this.apiKey;
+    }
+    public String getUrl() {
+        return this.url;
+    }
+    public AiApiModel getApiModel() {
+        return this.aiApiModel;
+    }
+    public String getResponse() {
+        return this.response;
+    }
+    public String getRequestBody() {
+        return this.requestBody;
+    }
+    //------------------------------Setters-----------------------------------
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public void setAiApiModel(AiApiModel aiApiModel) {
+        this.aiApiModel = aiApiModel;
+    }
+    public void setResponse(String response) {
+        this.response = response;
+    }
+    public void setRequestBody(String requestBody) {
+        this.requestBody = requestBody;
+    }
 }
--- Dad-Joke-API/src/main/java/org/falcon/jokegenerator/DadJokeGenerator.java
+++ Dad-Joke-API/src/main/java/org/falcon/jokegenerator/DadJokeGenerator.java
@@ -1,19 +1,20 @@
 package org.falcon.jokegenerator;
 
-
-import java.io.File;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 public class DadJokeGenerator {
     AiApiModel model;
     Joke joke;
+    //----------------------------Constructors--------------------------------
     public DadJokeGenerator() {
         this.buildModelFromJson();
         this.fetchJoke();
     }
-
+    //-------------------------Build-Model-From-Json--------------------------
     public void buildModelFromJson() {
-        //--------------------------Get-Data-From-Config----------------------
-        String configFilePath = "src/main/resources/ai_model_config.json";
+        //----------------------Get-Data-From-Config--------------------------
+        String configFilePath = "Dad-Joke-API/src/main/resources/config/ai_model_config.json";
         FileDataRetriever fileDataRetriever = new FileDataRetriever(configFilePath);
         String configFileContents = fileDataRetriever.getData();
         // Parse model
@@ -21,21 +22,57 @@ public class DadJokeGenerator {
         // Parse url
         String url = this.getValueFromKey(configFileContents, "urlPath");
         // Parse tokens
-        String tokens = this.getValueFromKey(configFileContents, "tokens");
+        String tokens = this.getValueFromKey(configFileContents, "maxTokens");
         // Parse temperature
         String temperature = this.getValueFromKey(configFileContents, "temperature");
         // Parse prompt
         String prompt = this.getValueFromKey(configFileContents, "fullPrompt");
         //--------------------------Build-Model-------------------------------
-
-
+        int tokensParsed = Integer.parseInt(tokens);
+        float temperatureParsed = Float.parseFloat(temperature);
+        this.model = new AiApiModel(model, prompt, tokensParsed, temperatureParsed);
     }
+    //---------------------------Get-Value-From-Key---------------------------
     public String getValueFromKey(String json, String key) {
-        return null;
+        // TODO: Implement regex parsing
+        // See src/main/resources/config/ai_model_config.json
+        return switch (key) {
+            case "model" -> "gpt-3.5-turbo-instruct";
+            case "urlPath" -> "https://api.openai.com/v1/completions";
+            case "maxTokens" -> "300";
+            case "temperature" -> "0.9";
+            case "fullPrompt" -> "Generate a dad joke which is started with" +
+                    " \\\"Joke: \\\" and if there is a punchline \\\"" +
+                    "Punchline: \\\".";
+            default -> throw new IllegalStateException("Unexpected value: " + key);
+        };
     }
+    //----------------------------Fetch-Joke----------------------------------
     public void fetchJoke() {
-        AiApiCall aiApiCall;
-        String response = aiApiCall.getResponse();
-        this.joke = new Joke(response);
+        boolean jokeIsUnique = false;
+        String previousJoke = "";
+        if (this.joke != null) {
+            previousJoke = this.joke.getFullJoke();
+        }
+        String keyFilePath = "Dad-Joke-Api/src/main/java/org/falcon/key.txt";
+        FileLineRetriever fileLineRetriever = new FileLineRetriever(0, keyFilePath);
+        String key = fileLineRetriever.getData();
+        String urlPath = this.getValueFromKey("", "urlPath");
+        while (!jokeIsUnique) {
+            AiApiCall aiApiCall = new AiApiCall(key, urlPath, this.model);
+            String response = aiApiCall.getResponse();
+            this.joke = new Joke(response);
+            if (!this.joke.getFullJoke().equals(previousJoke)) {
+                jokeIsUnique = true;
+            }
+        }
+    }
+    //------------------------------Getters-----------------------------------
+    public Joke getJoke() {
+        return this.joke;
+    }
+    //------------------------------Setters-----------------------------------
+    public void setJoke(Joke joke) {
+        this.joke = joke;
     }
 }
