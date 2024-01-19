f802020de74a13c251b195dcce9c63a0d0f7ae7e
Randy.F <randy.franzmeier@my.uwrf.edu>
2024-01-19 15:11:01 -0600

    Refactored script.js

--- Dad-Joke-API/src/main/resources/static/script.js
+++ Dad-Joke-API/src/main/resources/static/script.js
@@ -1,30 +1,31 @@
+//variables that manipulate the dom
 let generateButton = document.getElementById('joke-button-div');
 let generatedJoke = document.getElementById('generated-joke-div');
 let loadingWheel = document.getElementById('loading-wheel-img');
-const defaultButton = generateButton.outerHTML;
-generateButton.addEventListener('click', generateJoke);
-function attachListeners() {
-    generateButton = document.getElementById('joke-button-div');
-    generatedJoke = document.getElementById('generated-joke-div');
-    loadingWheel = document.getElementById('loading-wheel-img');
-    
+
+let attachListeners = () =>{
     generateButton.removeEventListener('click', generateJoke);
     generateButton.addEventListener('click', generateJoke);
 }
-function generateJoke() {
+let generateJoke = () => {
+    //hide the button and show the "loading" wheel
     generateButton.style.display = 'none';
     loadingWheel.style.display = 'block';
+    //set up request for api that displays at the correct location on
+    //the page upon successful retrieval
     let request = new XMLHttpRequest();
     request.open('GET', '/generate', true);
-    request.onload = function () {
+    request.onload = () =>{
         let fragResponse = request.responseText;
         generatedJoke.outerHTML = fragResponse;
         generateButton.style.display = 'block';
         loadingWheel.style.display = 'none';
-        attachListeners();
+        attachListeners();//make sure user can receive multiple jokes
     }
+    //send request
     request.send();
     
 
 }
+//attach event listeners on load
 attachListeners();
