b13285dc1164c111568356f077734b1733cf2394
theoliverlear <sigwartholiver2@gmail.com>
2024-04-02 15:20:05 -0500

    Add mapping for generating jokes with a given type of joke in DadJokeWebController.java

--- Dad-Joke-API/src/main/java/org/falcon/api/controller/DadJokeWebController.java
+++ Dad-Joke-API/src/main/java/org/falcon/api/controller/DadJokeWebController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DadJokeWebController {
+    String currentJokeType = "random";
     @Autowired
     private DadJokeService dadJokeService;
     @GetMapping("/")
@@ -18,8 +20,10 @@ public class DadJokeWebController {
         page.addAttribute("jokeText", joke);
         return "index";
     }
-    @GetMapping("/generate")
-    public String generate(Model page) {
+    @PostMapping("/generate/{jokeType}")
+    public String generate(Model page, @PathVariable String jokeType) {
+        this.currentJokeType = jokeType;
+        this.dadJokeService.getDadJokeGenerator().setJokeType(this.currentJokeType);
         this.dadJokeService.getDadJokeGenerator().fetchJoke();
         String joke = this.dadJokeService.getJoke();
         page.addAttribute("jokeText", joke);
