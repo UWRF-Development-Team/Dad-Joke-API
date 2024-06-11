8ad144b72e5f55c8f0ffa359f887b4bfc56a5dc7
Oliver Lear Sigwarth <sigwartholiver2@gmail.com>
2024-06-11 17:36:29 -0500

    Add get-path mapping based on selected dad in DadJokeWebController.java

--- Dad-Joke-API/src/main/java/org/falcon/api/controller/DadJokeWebController.java
+++ Dad-Joke-API/src/main/java/org/falcon/api/controller/DadJokeWebController.java
@@ -3,6 +3,8 @@ package org.falcon.api.controller;
 import org.falcon.api.service.DadJokeService;
 import org.falcon.jokegenerator.DadJokeGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DadJokeWebController {
+    public final String HOMER_IMG_PATH = "/audio/homer_sound_effect.mp3";
+    public final String PETER_IMG_PATH = "/audio/peter_sound_effect.mp3";
     String currentJokeType = "random";
     @Autowired
     private DadJokeService dadJokeService;
@@ -28,5 +32,15 @@ public class DadJokeWebController {
         String joke = this.dadJokeService.getJoke();
         page.addAttribute("jokeText", joke);
         return "joke-patch :: joke-patch";
+    }
+    @PostMapping("/get-path/{selectedDad}")
+    public ResponseEntity<String> getPath(@PathVariable String selectedDad) {
+        String[] dadNameSplit = selectedDad.split("-");
+        String dadName = dadNameSplit[0];
+        if (dadName.equals("homer")) {
+            return new ResponseEntity<>(this.HOMER_IMG_PATH, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(this.PETER_IMG_PATH, HttpStatus.OK);
+        }
     }
 }
